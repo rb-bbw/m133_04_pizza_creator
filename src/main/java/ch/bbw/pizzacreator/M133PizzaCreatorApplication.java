3891107bc3cc06c88e6752855b448a9d1967f7b8
rb-bbw <remo.bergamin@lernende.bbw.ch>
2022-04-12 11:57:09 +0200

    Init

---
+++ src/main/java/ch/bbw/pizzacreator/M133PizzaCreatorApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.pizzacreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M133PizzaCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M133PizzaCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/pizzacreator/M133PizzaCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.pizzacreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M133PizzaCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
